acad64a47100e3caecea9091f7d5c38c9cadb448
Matt <macavall@microsoft.com>
2025-09-03 14:28:37 -0500

    first

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = FunctionsApplication.CreateBuilder(args);
+
+        builder.ConfigureFunctionsWebApplication();
+
+        builder.Services
+            .AddApplicationInsightsTelemetryWorkerService()
+            .ConfigureFunctionsApplicationInsights();
+
+
+        builder.Build().Run();
+    }
+}
